5e4b802d13914f9b88f371e9cb5dc66f372a7ffd
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-03-23 18:49:14 -0300

    feat: add in memories a page to create a new one.

---
+++ app/index.tsx
@@ -0,0 +1,89 @@
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+import { api } from '../src/lib/api'
+import NLWLogo from '../src/assets/nlw-logo.svg'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/f8a1cc211fd4c249497e',
+}
+
+export default function App() {
+  const router = useRouter()
+
+  const [, response, signInWithGitHub] = useAuthRequest(
+    {
+      clientId: 'f8a1cc211fd4c249497e',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGitHubOAuth(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGitHubOAuth(code)
+    }
+  }, [response])
+
+  return (
+    <View className="flex-1 items-center justify-center px-8 py-10">
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => signInWithGitHub()}
+        >
+          <Text className="font-alt text-sm uppercase leading-tight text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </View>
+  )
+}
